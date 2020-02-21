d848a4522f3e7301b158271ac191cfbe110fd177
Prudhvi-Nath <41383256+Prudhvi-Nath@users.noreply.github.com>
2020-02-21 18:53:19 +0530

    Delete database.sql

--- database.sql
+++
@@ -1,5 +0,0 @@
-drop table bankaccholder;
-set pagesize 200
-set linesize 200
-set num 10
-create table bankaccholder(id number(6) constraint pk_id primary key,accholdername varchar2(30),password varchar2(30),accbalance number(15),typeofaccount varchar2(30));
